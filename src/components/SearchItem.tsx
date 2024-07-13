5845e5bdf3d58d6c7da09cc740565aa50087b01c
Felipe Melo <felipe.melo@terceiro-sky.com.br>
2024-07-13 11:03:21 -0300

    remove stylesheet

--- src/components/SearchItem.tsx
+++ src/components/SearchItem.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { StyleSheet } from "react-native";
-import { Box, Input } from "native-base";
+import { Box, Input } from "@gluestack-ui/themed-native-base";
 
 interface SearchItemProps {
   search: string;
@@ -14,23 +13,17 @@ export const SearchItem: React.FC<SearchItemProps> = ({
   return (
     <Box>
       <Input
-        style={styles.input}
         placeholder="Search items"
         value={search}
         onChangeText={setSearch}
         variant="outline"
+        height={10} 
+        borderColor="#ccc"
+        borderWidth={1}
+        borderRadius={8}
+        paddingLeft={2}
+        paddingRight={2} 
       />
     </Box>
   );
 };
-
-const styles = StyleSheet.create({
-  input: {
-    height: 40,
-    borderColor: "#ccc",
-    borderWidth: 1,
-    borderRadius: 8,
-    paddingLeft: 8,
-    paddingRight: 8,
-  },
-});
